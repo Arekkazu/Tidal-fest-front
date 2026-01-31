04b1996eaec0631b416335cb5067b9451b5ea429
Alexander Lozada Caviedes <99624129+Arekkazu@users.noreply.github.com>
2026-01-31 13:14:47 -0500

    Add Google site verification meta tag

--- app/root.tsx
+++ app/root.tsx
@@ -29,6 +29,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="z94w7dPw8PNqD9YmkX14ozh3RZnqbT8cfCSjIzXEjXg" />
         <Meta />
         <Links />
       </head>
